95cf9f796fb17058a2233bea68ac926eae5c68b7
NLejbman <nlejbman@frba.utn.edu.ar>
2023-06-06 22:10:53 -0300

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,29 +1,19 @@
 #include <iostream>
 
-#include <math.h>
-
 using namespace std;
 
-int main()
+int main() {
+    double aprox_pi = 0.0;
+    double denominador = 1.0;
+    int signo = 1;
 
-{
+    while (aprox_pi < 3.141592 || aprox_pi > 3.141593) {
+        aprox_pi += signo * (4.0 / denominador);
+        denominador += 2.0;
+        signo *= -1;
+    }
+    cout.precision(6);
+    cout << "El valor de pi es: " <<fixed <<aprox_pi <<endl;
 
-double aprox_pi = 0; 
-
-int iteraciones = 1000000;/*cambiar el numero para mas precicision*/
-
-for (long int n = 1; n <= iteraciones; n++)
-
-{
-
-aprox_pi+= (double) pow(-1, n+1)/(2*n-1);
-
-}
-
-aprox_pi *= 4;
-
-cout << "Estimacion de PI (" << iteraciones<< " iteraciones): "<< aprox_pi<<endl;
-
-return 0;
-
+    return 0;
 }
